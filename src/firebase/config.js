b2e0c1006fa3ee31360630d2e7fbf5685d7bf13a
shahrazad <shahrazadkhowaja@gmail.com>
2025-02-09 00:58:07 +0500

    Secure Firebase Configuration

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLcN2NhaslA6MOTU25EGEwSfFOudNEtPo",
-  authDomain: "crudwithfirebase-c20c9.firebaseapp.com",
-  projectId: "crudwithfirebase-c20c9",
-  storageBucket: "crudwithfirebase-c20c9.firebasestorage.app",
-  messagingSenderId: "294542475166",
-  appId: "1:294542475166:web:c76e81dc480daf2d5be08f",
-  measurementId: "G-F9D49GKN5R"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  StorageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export { app };
